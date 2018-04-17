fb7cfcaf5b45c8e60e6baa1c51cc2a43b70d0c9e
Tika Raj Bista <tikarajbista@gmail.com>
2018-04-17 12:07:51 -0500

    initialized LoginActivity and RegisterActivity

--- app/src/main/java/com/hillygeeks/mdpproject/LoginActivity.java
+++ app/src/main/java/com/hillygeeks/mdpproject/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.hillygeeks.mdpproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // Method to start RegisterActivity when one clicks on Register
+    public void onRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/hillygeeks/mdpproject/RegisterActivity.java
+++ app/src/main/java/com/hillygeeks/mdpproject/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.hillygeeks.mdpproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,4 +11,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+
 }
