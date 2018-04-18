73176f37e66bc860bcdb85e769fa8c6a285dc2df
Tika Raj Bista <tika.raj.bista@gmail.com>
2018-04-17 22:53:48 -0600

    Integrated Firebase and RegisterActivity is done!

--- app/src/main/java/com/hillygeeks/mdpproject/LoginActivity.java
+++ app/src/main/java/com/hillygeeks/mdpproject/LoginActivity.java
@@ -1,17 +1,112 @@
 package com.hillygeeks.mdpproject;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Firebase
+    private FirebaseAuth.AuthStateListener listener;
+
+    // widgets
+    private EditText mEmail, mPassword;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = (EditText) findViewById(R.id.txt_email);
+        mPassword = (EditText) findViewById(R.id.txt_pass);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        setupFirebaseAuth();
+
+        Button signIn = (Button) findViewById(R.id.btn_signin);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isEmpty(mEmail.getText().toString())
+                        && !isEmpty(mPassword.getText().toString())){
+
+                    showDialog();
+
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail.getText().toString(),
+                            mPassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    hideDialog();
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            hideDialog();
+                        }
+                    });
+                }else{
+                    Toast.makeText(LoginActivity.this, "You didn't fill in all the fields.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        hideSoftKeyboard();
     }
+
+    private void setupFirebaseAuth() {
+        listener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "", Toast.LENGTH_SHORT).show();Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // User is signed out
+                    Toast.makeText(LoginActivity.this, "Failed to logged!", Toast.LENGTH_SHORT).show();
+                }
+                // ...
+            }
+        };
+    }
+
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
 
     // Method to start RegisterActivity when one clicks on Register
     public void onRegister(View view) {
